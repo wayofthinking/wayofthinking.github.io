c7094f03b207ec2631b03f0a5f4ae855f1f2573d
Thierry de Pauw <thierry@thinkinglabs.io>
2020-06-26 20:48:19 +0200

    Add DKIM record for wayofthinking.eu

--- dns/main.tf
+++ dns/main.tf
@@ -47,6 +47,7 @@ locals {
   spf_record = "\"v=spf1 include:_spf.google.com ~all\""
   dkim_net   = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCobrYREvBte0muos5K/UJSErNMG0vC3KYpMIvvCP9cXtl0y6xK+2GipfKWQ3Oc0nGng9kxqI6WYyOjJ4nH6+Nc0OhfyEg2YnyBbBtBYqzdxQSoovgE2cGcpCk7X1jeXgVv60RQwNQ2C9ZlGj5v+pVW0JcPSH9s9Gtuf5y/t150cQIDAQAB"
   dkim_be    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsbmGNLcdeTBfyhkk/tcLodYqKQkg3KoGJtNEuiByI+ESg0i/QzbKTqBvwRF77RDgXE79iooFfWp/Kb/UTNgmUK7V+maqO/r+R8o/TfcK/C2sKRc6+QXXSDBXflYsKe1tpyKu/j3oGAGl8HMt1kcnunOtkMYNPF+Ic5A4PFmReJQIDAQAB"
+  dkim_eu    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCEu0esc/eS0zVBXpvHUQNSKM6nxRWDbOfn56BwB10ou3qwv8K81GFpGKqp3gDMRkbDuw6eRc2RTaHxKGrQreGRvGVv1RLF5TMcjVFKXOihukuRoVt4uPV8pzG5nGgBkPHZgqh1ZDHOFgFrtCVbxxBF45EyuloZu6cOEy9GnAM7fQIDAQAB"
 }
 
 resource "ovh_domain_zone_record" "net_name_server" {
@@ -214,6 +215,14 @@ resource "ovh_domain_zone_record" "eu_spf" {
   fieldtype = "TXT"
   ttl       = local.ttl_spf
   target    = local.spf_record
+}
+
+resource "ovh_domain_zone_record" "eu_dkim" {
+  zone      = local.zone_eu
+  subdomain = "google._domainkey"
+  fieldtype = "TXT"
+  ttl       = local.ttl_spf
+  target    = local.dkim_eu
 }
 
 resource "ovh_domain_zone_record" "eu_gsuite_mail" {
