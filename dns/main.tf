a656d0075a2056e1b57837cea032540e8ba08181
Thierry de Pauw <thierry@thinkinglabs.io>
2020-06-27 19:22:02 +0200

    Reformat

--- dns/main.tf
+++ dns/main.tf
@@ -44,20 +44,22 @@ locals {
     { subdomain = "mail", target = "ghs.googlehosted.com." }
   ]
 
-  spf        = "\"v=spf1 include:_spf.google.com ~all\""
-  dkim_net   = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCobrYREvBte0muos5K/UJSErNMG0vC3KYpMIvvCP9cXtl0y6xK+2GipfKWQ3Oc0nGng9kxqI6WYyOjJ4nH6+Nc0OhfyEg2YnyBbBtBYqzdxQSoovgE2cGcpCk7X1jeXgVv60RQwNQ2C9ZlGj5v+pVW0JcPSH9s9Gtuf5y/t150cQIDAQAB"
-  dkim_be    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsbmGNLcdeTBfyhkk/tcLodYqKQkg3KoGJtNEuiByI+ESg0i/QzbKTqBvwRF77RDgXE79iooFfWp/Kb/UTNgmUK7V+maqO/r+R8o/TfcK/C2sKRc6+QXXSDBXflYsKe1tpyKu/j3oGAGl8HMt1kcnunOtkMYNPF+Ic5A4PFmReJQIDAQAB"
-  dkim_eu    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCEu0esc/eS0zVBXpvHUQNSKM6nxRWDbOfn56BwB10ou3qwv8K81GFpGKqp3gDMRkbDuw6eRc2RTaHxKGrQreGRvGVv1RLF5TMcjVFKXOihukuRoVt4uPV8pzG5nGgBkPHZgqh1ZDHOFgFrtCVbxxBF45EyuloZu6cOEy9GnAM7fQIDAQAB"
+  spf      = "\"v=spf1 include:_spf.google.com ~all\""
+  dkim_net = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCobrYREvBte0muos5K/UJSErNMG0vC3KYpMIvvCP9cXtl0y6xK+2GipfKWQ3Oc0nGng9kxqI6WYyOjJ4nH6+Nc0OhfyEg2YnyBbBtBYqzdxQSoovgE2cGcpCk7X1jeXgVv60RQwNQ2C9ZlGj5v+pVW0JcPSH9s9Gtuf5y/t150cQIDAQAB"
+  dkim_be  = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsbmGNLcdeTBfyhkk/tcLodYqKQkg3KoGJtNEuiByI+ESg0i/QzbKTqBvwRF77RDgXE79iooFfWp/Kb/UTNgmUK7V+maqO/r+R8o/TfcK/C2sKRc6+QXXSDBXflYsKe1tpyKu/j3oGAGl8HMt1kcnunOtkMYNPF+Ic5A4PFmReJQIDAQAB"
+  dkim_eu  = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCEu0esc/eS0zVBXpvHUQNSKM6nxRWDbOfn56BwB10ou3qwv8K81GFpGKqp3gDMRkbDuw6eRc2RTaHxKGrQreGRvGVv1RLF5TMcjVFKXOihukuRoVt4uPV8pzG5nGgBkPHZgqh1ZDHOFgFrtCVbxxBF45EyuloZu6cOEy9GnAM7fQIDAQAB"
 }
 
 module "net" {
   source = "./modules/zone"
 
-  zone                     = local.zone_net
-  name_servers             = local.name_servers
+  zone         = local.zone_net
+  name_servers = local.name_servers
+
   google_site_verification = "2V4XwMeXE8SYmcHAQ30NlAUArvR8NFgotefUmO-4x2c"
-  mx = local.gsuite_mx_records_for_net
-  spf = local.spf
+
+  mx   = local.gsuite_mx_records_for_net
+  spf  = local.spf
   dkim = local.dkim_net
 }
 
@@ -66,9 +68,11 @@ module "be" {
 
   zone         = local.zone_be
   name_servers = local.name_servers
+
   google_site_verification = "Z85qsHhGDqO317DaUZRgMeCGH44FlJz333T_wgRjiPE"
-  mx = local.gsuite_mx_records_for_be
-  spf = local.spf
+
+  mx   = local.gsuite_mx_records_for_be
+  spf  = local.spf
   dkim = local.dkim_be
 }
 
@@ -77,9 +81,11 @@ module "eu" {
 
   zone         = local.zone_eu
   name_servers = local.name_servers
+
   google_site_verification = "jncyCZipyOxhCFlrpp1UgSVFeqWAXBCp7Dowv8vnZ_w"
-  mx = local.gsuite_mx_records_for_be
-  spf = local.spf
+
+  mx   = local.gsuite_mx_records_for_be
+  spf  = local.spf
   dkim = local.dkim_eu
 }
 
